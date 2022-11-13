1367a1c69e84334f7e04910a103bc308a4602640
Alejandro Rojas <kmachappy@pm.me>
2022-11-13 13:12:06 -0800

    testing commit

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -12,16 +12,14 @@ const LayoutWrapper = ({ children }) => {
     <SectionContainer>
       <div className="flex h-screen flex-col justify-between">
         <header className="flex items-center justify-between py-10">
-          <div>
+          <div className="">
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
                 {/* <div className="mr-3">
                   <Logo />
                 </div> */}
                 {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
-                  </div>
+                  <div className="text-2xl font-semibold sm:block">{siteMetadata.headerTitle}</div>
                 ) : (
                   siteMetadata.headerTitle
                 )}
