f5afa8fb608237746be7234568a683ef931e6e97
Alejandro Rojas <kmachappy@pm.me>
2022-11-14 01:22:59 -0800

    asdfs

--- next.config.js
+++ next.config.js
@@ -66,22 +66,22 @@ module.exports = withBundleAnalyzer({
       },
     ]
   },
-  webpack: (config, { dev, isServer }) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    })
+  // webpack: (config, { dev, isServer }) => {
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     use: ['@svgr/webpack'],
+  //   })
 
-    if (!dev && !isServer) {
-      // Replace React with Preact only in client production build
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
+  //   if (!dev && !isServer) {
+  //     // Replace React with Preact only in client production build
+  //     Object.assign(config.resolve.alias, {
+  //       'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+  //       react: 'preact/compat',
+  //       'react-dom/test-utils': 'preact/test-utils',
+  //       'react-dom': 'preact/compat',
+  //     })
+  //   }
 
-    return config
-  },
+  //   return config
+  // },
 })
